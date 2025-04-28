9560db661b4d92b1ad4debc6c30d44e0b1a72caa
pedroarthurSFTW <pedroarthur0017@hotmail.com>
2025-04-27 22:27:02 -0300

    create routes employee

--- src/main/java/com/example/aureus/controller/EmployeeController.java
+++ src/main/java/com/example/aureus/controller/EmployeeController.java
@@ -1,11 +1,45 @@
 package com.example.aureus.controller;
 
+import com.example.aureus.dto.employee.EmployeeRequestDTO;
+import com.example.aureus.dto.employee.EmployeeResponseDTO;
+import com.example.aureus.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/employee")
 @RequiredArgsConstructor
 public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeResponseDTO> getEmployee(@PathVariable UUID id) {
+        EmployeeResponseDTO employee = employeeService.getEmployeeById(id);
+        if (employee == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(employee);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeResponseDTO>> getAllEmployees() {
+        List<EmployeeResponseDTO> employees = employeeService.getAllEmployees();
+        if (employees == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(employees);
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeResponseDTO> createEmployee(@RequestBody EmployeeRequestDTO employeeRequestDTO) {
+        EmployeeResponseDTO employee = employeeService.createEmployee(employeeRequestDTO);
+        if (employee == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(employee);
+    }
 }
