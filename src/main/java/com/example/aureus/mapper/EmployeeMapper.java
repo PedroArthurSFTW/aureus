9641052ce51f29d6f394e9bf0f84671765f5ba5c
pedroarthurSFTW <pedroarthur0017@hotmail.com>
2025-04-29 08:35:45 -0300

    refactor: remove redundant code

--- src/main/java/com/example/aureus/mapper/EmployeeMapper.java
+++ src/main/java/com/example/aureus/mapper/EmployeeMapper.java
@@ -4,11 +4,9 @@ import com.example.aureus.dto.employee.EmployeeRequestDTO;
 import com.example.aureus.dto.employee.EmployeeResponseDTO;
 import com.example.aureus.model.Employee;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface EmployeeMapper {
-    EmployeeMapper INSTANCE = Mappers.getMapper(EmployeeMapper.class);
     Employee toEntity(EmployeeRequestDTO employeeRequestDTO);
     EmployeeResponseDTO toDTO(Employee employee);
 }
